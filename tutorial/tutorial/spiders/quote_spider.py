2622a81934d2a893126ada743a9dc38c2e89c325
Paula Coll <paula.coll@bbva.com>
2021-06-16 08:07:07 +0200

    Include some notes with reminders about parts of the code.

--- tutorial/tutorial/spiders/quote_spider.py
+++ tutorial/tutorial/spiders/quote_spider.py
@@ -2,9 +2,24 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
+    """
+    Sample spider from the Scrapy tutorial.
+    In order to run it, execute scrapy crawl quotes
+    """
+
+    # The name of the spider must be unique within the project. All spiders go into the /spiders folder
     name = "quotes"
 
+    # Either define a start_requests method or the following variable (commented as a reminder)
+    # start_urls = [
+    #     'http://quotes.toscrape.com/page/1/',
+    #     'http://quotes.toscrape.com/page/2/',
+    # ]
+
     def start_requests(self):
+        """
+        Generator that returns an iterable of Requests that the spider will crawl from
+        """
         urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
@@ -13,6 +28,9 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        """
+        Scrapy's default callback method
+        """
         page = response.url.split("/")[-2]
         filename = f'quotes-{page}.html'
         with open(filename, 'wb') as f:
