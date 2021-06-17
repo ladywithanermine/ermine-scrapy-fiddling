81b5238735260bd4bcfbb1a43fe48f5d75e7e031
Paula Coll <paula.coll@bbva.com>
2021-06-17 08:43:06 +0200

    Add author spider. Disable quotes pipeline.

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -63,7 +63,8 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'tutorial.pipelines.TutorialPipeline': 300,
+    #'tutorial.pipelines.TutorialPipeline': 300,
+    
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
